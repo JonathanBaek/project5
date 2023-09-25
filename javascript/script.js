10cba997401efefdc7865c37a63d823a5742aaec
JonathanBaek <141523844+JonathanBaek@users.noreply.github.com>
2023-09-24 22:55:32 -0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,19 @@ window.addEventListener("scroll", () => {
 
     lastScrollY = window.scrollY;
 });
+
+
+const body = document.querySelector('body');
+const modeToggle = document.getElementById('mode-toggle');
+const modeStatus = document.querySelector('.mode-status');
+
+function toggleMode() {
+  body.classList.toggle('dark-mode');
+
+  const modeMessage = body.classList.contains('dark-mode') ?
+    "dark" 
+    : "light"
+
+  modeStatus.innerText = modeMessage;
+}
+modeToggle.addEventListener('click', toggleMode);
